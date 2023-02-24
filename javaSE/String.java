d2e3b373044c8f8a822c42472cdf4284b8403d73
Zhengjian Kang <zk299@nyu.edu>
2023-02-23 21:35:17 -0500

    Create String.java

---
+++ javaSE/String.java
@@ -0,0 +1,9 @@
+//2. String
+String str = new String("ab666");
+str = new String(new char[]{'a', 'b', '6', '6', '6'});
+// common method
+System.out.println("2. String");
+System.out.println(str); // ab666
+System.out.println(str.charAt(0)); // 'a'
+System.out.println(str.substring(1,2)); //[i,j) 'b'
+System.out.println("a".compareTo("b")); //-1 "a" < "b"
