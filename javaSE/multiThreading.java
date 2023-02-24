67d486ffc48de9012c8e4001914a9f7fac940885
Zhengjian Kang <zk299@nyu.edu>
2023-02-24 14:59:51 -0500

    Create multiThreading.java

---
+++ javaSE/multiThreading.java
@@ -0,0 +1,21 @@
+// Create thread using futureTask
+// FutureTask implements Runnable & Future interface, and allows to pass Callable interface to get result
+
+FutureTask<String> task = new FutureTask<>(() -> {
+    Thread.sleep(1000);
+    return "Complete";
+});
+Thread th = new Thread(task);
+th.start();
+System.out.println(task.get());
+
+
+// 
+ExecutorService executorService = Executors.newSingleThreadExecutor();
+Future<String> task = executorService.submit(() -> {
+    Thread.sleep(1000);
+    return "Complete";
+});
+
+System.out.println(task.get());
+
