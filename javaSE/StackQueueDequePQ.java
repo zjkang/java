52c8656d6f1579376f1c3eac6ab9789f426865fb
Zhengjian Kang <zk299@nyu.edu>
2023-02-23 22:15:15 -0500

    Create StackQueueDequePQ.java

---
+++ javaSE/StackQueueDequePQ.java
@@ -0,0 +1,12 @@
+ //6. Stack
+ Deque<Integer> stack = new ArrayDeque<>();
+ stack.push(1);
+ stack.push(2);
+ stack.push(3);
+ System.out.println(stack.peek()); // return 3
+ System.out.println(stack.pop()); // return 3
+ System.out.println(stack.pop()); // return 2
+ System.out.println(stack.isEmpty()); // false
+
+
+
