69be549403db593b88174f6b8f83f7178f839741
Zhengjian Kang <zk299@nyu.edu>
2023-02-23 22:17:20 -0500

    Update StackQueueDequePQ.java

--- javaSE/StackQueueDequePQ.java
+++ javaSE/StackQueueDequePQ.java
@@ -9,4 +9,16 @@
  System.out.println(stack.isEmpty()); // false
 
 
+//7. Queue
+Deque<Integer> queue = new ArrayDeque<>();
+queue.offer(1);
+queue.offer(2);
+queue.offer(3);
+System.out.println(queue.peek()); // return 1, not remove
+System.out.println(queue.poll()); // return 1, remove, poll()处理nul，不会报错
+System.out.println(queue.peek()); // return 2, not remove
+System.out.println(queue.isEmpty()); // false
+
+
+
 
