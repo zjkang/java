34f33c5357cb992e6cc6c712e895157701f4a321
Zhengjian Kang <zk299@nyu.edu>
2023-02-23 22:29:30 -0500

    Update StackQueueDequePQ.java

--- javaSE/StackQueueDequePQ.java
+++ javaSE/StackQueueDequePQ.java
@@ -20,5 +20,30 @@ System.out.println(queue.peek()); // return 2, not remove
 System.out.println(queue.isEmpty()); // false
 
 
+//8. Deque
+Deque<Integer> deque = new ArrayDeque<>();
+deque.offerLast(1); // 1
+deque.offerLast(2); // 1->2
+deque.offerFirst(0); // 0->1->2
+System.out.println(deque.peekFirst()); // return 0
+System.out.println(deque.pollFirst()); // return 0
+System.out.println(deque.pollLast()); // return 2
 
+
+//9. Priority Queue (Heap)
+PriorityQueue<Integer> pq = new PriorityQueue<>(); // default min-heap
+// PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // max-heap
+// PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b); // lambda function
+// PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() { // override comparator
+//     @Override
+//     public int compare(Integer o1, Integer o2) {
+//         return o1.compareTo(o2);
+//     }
+// });
+pq.offer(2);
+pq.offer(0);
+pq.offer(1);
+System.out.println(pq.peek()); // return 0 O(1)
+System.out.println(pq.poll()); // return 0 O(logn)
+System.out.println(pq.poll()); // return 1 O(logn)
 
