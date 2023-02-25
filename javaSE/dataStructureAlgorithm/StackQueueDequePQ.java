5a40bb8fc161b16ac21e16a755e9322719f027a5
Zhengjian Kang <zk299@nyu.edu>
2023-02-25 12:29:23 -0500

    Update StackQueueDequePQ.java

--- javaSE/dataStructureAlgorithm/StackQueueDequePQ.java
+++ javaSE/dataStructureAlgorithm/StackQueueDequePQ.java
@@ -1,4 +1,4 @@
- //6. Stack
+ // Stack
  Deque<Integer> stack = new ArrayDeque<>();
  stack.push(1);
  stack.push(2);
@@ -9,7 +9,7 @@
  System.out.println(stack.isEmpty()); // false
 
 
-//7. Queue
+// Queue
 Deque<Integer> queue = new ArrayDeque<>();
 queue.offer(1);
 queue.offer(2);
@@ -20,7 +20,7 @@ System.out.println(queue.peek()); // return 2, not remove
 System.out.println(queue.isEmpty()); // false
 
 
-//8. Deque
+// Deque
 Deque<Integer> deque = new ArrayDeque<>();
 deque.offerLast(1); // 1
 deque.offerLast(2); // 1->2
@@ -30,16 +30,18 @@ System.out.println(deque.pollFirst()); // return 0
 System.out.println(deque.pollLast()); // return 2
 
 
-//9. Priority Queue (Heap)
+// Priority Queue (Heap)
 PriorityQueue<Integer> pq = new PriorityQueue<>(); // default min-heap
-// PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // max-heap
-// PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b); // lambda function
-// PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() { // override comparator
-//     @Override
-//     public int compare(Integer o1, Integer o2) {
-//         return o1.compareTo(o2);
-//     }
-// });
+PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder()); // max-heap
+PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a - b); // lambda function
+PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> a.compareTo(b)); // lambda function
+PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() { // override comparator
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o1.compareTo(o2);
+    }
+});
+
 pq.offer(2);
 pq.offer(0);
 pq.offer(1);
