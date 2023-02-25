71bc4edb81b0cdfc762abce2050fd798bfebb811
Zhengjian Kang <zk299@nyu.edu>
2023-02-25 11:10:33 -0500

    Update String.java

--- javaSE/String.java
+++ javaSE/String.java
@@ -7,3 +7,10 @@ System.out.println(str); // ab666
 System.out.println(str.charAt(0)); // 'a'
 System.out.println(str.substring(1,2)); //[i,j) 'b'
 System.out.println("a".compareTo("b")); //-1 "a" < "b"
+
+String[] names = new String[]{"a", "b", "c"};
+StringBuilder nameBuilder = new StringBuilder();
+for (String name : names) {
+    nameBuilder.append(name);
+}
+System.out.println(nameBuilder);
