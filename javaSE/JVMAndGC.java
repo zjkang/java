59143b8b052f70e5093e2dafea6aa9e268e01690
Zhengjian Kang <zk299@nyu.edu>
2023-02-26 12:25:50 -0500

    Create JVMAndGC.java

---
+++ javaSE/JVMAndGC.java
@@ -0,0 +1,14 @@
+Q21. What are the different types of garbage collectors in Java?
+
+Garbage collection in Java a program which helps in implicit memory management. 
+Since in Java, using the new keyword you can create objects dynamically, 
+which once created will consume some memory. Once the job is done and there are 
+no more references left to the object, Java using garbage collection destroys the object 
+and relieves the memory occupied by it.
+
+Java provides four types of garbage collectors:
+
+Serial Garbage Collector
+Parallel Garbage Collector
+CMS Garbage Collector
+G1 Garbage Collector
