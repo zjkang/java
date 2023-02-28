ba2e7439b1efb24233a8d5ed74747275d590eeb9
Zhengjian Kang <zk299@nyu.edu>
2023-02-28 16:12:20 -0500

    Update multiThreading.java

--- javaSE/multiThreading.java
+++ javaSE/multiThreading.java
@@ -22,5 +22,57 @@ System.out.println(task.get());
 
 
 // wait or notify
-https://www.baeldung.com/java-wait-notify
+// https://www.baeldung.com/java-wait-notify
+// example: print 0,1,2,...9 using multi-threading to keep in order
+
+package thread.sync;
+
+public class PrintSync {
+    public static void main(String[] args) {
+        var ref = new Object() {
+            int flag = 0;
+        };
+        Object lock = new Object();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10; i += 2) {
+                synchronized (lock) {
+                    while (ref.flag != 0) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(i);
+                    ref.flag = 1;
+                    lock.notify();
+                }
+            }
+        }, "t1");
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 1; i < 10; i += 2) {
+                synchronized (lock) {
+                    while (ref.flag != 1) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(i);
+                    ref.flag = 0;
+                    lock.notify();
+                }
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+    }
+}
+
+
+
+
 
