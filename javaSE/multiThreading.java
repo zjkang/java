48c19957a61903bf5b2457188ef6fe9716d3478f
Zhengjian Kang <zk299@nyu.edu>
2023-02-28 17:26:17 -0500

    Update multiThreading.java

--- javaSE/multiThreading.java
+++ javaSE/multiThreading.java
@@ -1,3 +1,24 @@
+// Thread basic concept
+https://www.javatpoint.com/java-multithreading-interview-questions
+  9) What is the difference between preemptive scheduling and time slicing?
+      Under preemptive scheduling, the highest priority task executes until it enters the waiting or dead states or a higher priority task comes into existence. 
+      Under time slicing, a task executes for a predefined slice of time and then reenters the pool of ready tasks. 
+      The scheduler then determines which task should execute next, based on priority and other factors.
+  10) What is context switching?
+  18)Can we make the user thread as daemon thread if the thread is started?
+  19)What is shutdown hook? vs finalizer()
+  21) What is the synchronization?
+    Synchronization is the capability to control the access of multiple threads to any shared resource
+  30) How is the safety of a thread achieved?
+      If a method or class object can be used by multiple threads at a time without any race condition, then the class is thread-safe. Thread safety is used to make a program safe to use in multithreaded programming. It can be achieved by the following ways:
+        Synchronization
+        Using Volatile keyword
+        Using a lock based mechanism
+        Use of atomic wrapper classes
+  35) What is the Executor interface in Concurrency API in Java?
+      
+
+
 // Create thread using futureTask
 // FutureTask implements Runnable & Future interface, and allows to pass Callable interface to get result
 
