61f9f3fcdcee3e6caafb18c6d28ef8c7fc61e8f9
Zhengjian Kang <zk299@nyu.edu>
2023-03-01 12:23:46 -0500

    Update String.java

--- javaSE/dataStructureAlgorithm/String.java
+++ javaSE/dataStructureAlgorithm/String.java
@@ -14,3 +14,22 @@ for (String name : names) {
     nameBuilder.append(name);
 }
 System.out.println(nameBuilder);
+
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            // String key = new String(chars);
+            String key = String.valueOf(chars); // convert char[] to String
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList(map.values());  // convert Collection<List<String>> to List<List<String>>
+    }
+}
