bf317610e2ca84a5c79e73f64836f0b80c6519cc
Zhengjian Kang <zk299@nyu.edu>
2023-03-05 22:24:29 -0500

    Update List03062023.java

--- javaSE/everyday/List03062023.java
+++ javaSE/everyday/List03062023.java
@@ -4,11 +4,19 @@ List<String> list = new ArrayList<>();
 list.add("AAA");
 list.add("BBB");
 
+
+// What's difference between Arrays.asList and List.of
+// Arrays.asList return reference to fixed size list. it throws an exception when add, but the value can be updated
+// List.of return reference to unmodifiable list, java.lang.UnsupportedOperationException
 List<String> list = Arrays.asList("AAA", "BBB");
 list.set(0, "CCC");
 list.add("CCC"); // cannot add element
 
 List<String> list = List.of("AAA", "BBB");
 list.set(0, "CCC"); // cannot modify
+list.add("CCC"); // cannot add element
+
+// What if I need to add using Arrays.asList or List.of?
+List<String> list = new ArrayList<>(Arrays.asList("AAA", "BBB"));
 list.add("CCC"); // cannot add element
 
