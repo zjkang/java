59fdb3706ddb689cee2be2fbfa539ad6b179b791
Zhengjian Kang <zk299@nyu.edu>
2023-03-07 10:43:33 -0500

    Update 03072023-CombineCollections.java

--- javaSE/everyday/03072023-CombineCollections.java
+++ javaSE/everyday/03072023-CombineCollections.java
@@ -1,1 +1,21 @@
 // https://www.baeldung.com/java-combine-multiple-collections
+
+// how to combine 2 collections into one
+Collection<String> collectionA = Arrays.asList("S", "T");
+Collection<String> collectionB = Arrays.asList("U", "V");
+
+// Java 8
+// method 1 using Stream.concat()
+Stream<String> combinedStream = Stream.concat(
+  collectionA.stream(),
+  collectionB.stream());
+
+Stream<String> combinedStream = Stream.of(collectionA, collectionB)
+  .flatMap(Collection::stream);
+Collection<String> collectionCombined = 
+  combinedStream.collect(Collectors.toList());
+
+// Java 7
+List<String> listC = new ArrayList<>();
+listC.addAll(listA);
+listC.addAll(listB);
