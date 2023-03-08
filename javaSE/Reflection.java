c0152968b80ef0c782ee39b287ef591f7a21d07a
Zhengjian Kang <zk299@nyu.edu>
2023-03-08 17:54:36 -0500

    Update Reflection.java

--- javaSE/Reflection.java
+++ javaSE/Reflection.java
@@ -1,2 +1,14 @@
 //
-//
+// Class<?>: (wildcard ?)
+// Java wildcard: ? for generic type
+// 1. Class<Object> objectClass = object.getClass();
+// 2. Class<Car> carClass = Car.class;
+//   ".class" suffix to a type name
+//   Class booleanType = boolean.class;
+// 3. Class.forName("")
+// List<?>  <-- List<T> extends
+
+Constructor<?> class
+1. Class.getDeclaredConstructors(): include public or not
+
+
