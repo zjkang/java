f97b604a0858ed9f6f64fdfeec8fa22fc8e8cb0e
Zhengjian Kang <zk299@nyu.edu>
2023-03-08 16:39:46 -0500

    Create SetMap.java

---
+++ javaSE/everyday/SetMap.java
@@ -0,0 +1,53 @@
+// Hash Map
+Map<String, Integer> map = new HashMap<>();
+map.put("A", 0); // <key, value>
+map.put("B", 1);
+map.put("C", 2);
+System.out.println(map.get("A")); // return 0
+System.out.println(map.get("B")); // return 1
+System.out.println(map.containsKey("B")); // return true
+System.out.println(map.getOrDefault("B", 2)); // return 1
+System.out.println(map.getOrDefault("D", 10)); // return 10
+System.out.println(map); //{A=0,B=1,C=2}
+
+for(Map.Entry<String, Integer> entry : map.entrySet()) {
+   System.out.println(entry.getKey() + ":" + entry.getValue());
+}
+// map.putIfAbsent("D", 3); // only add once key D does not in map
+// map.computeIfAbsent();
+// map.computeIfPresent();
+
+
+// Hash Set
+Set<Integer> set = new HashSet<>();
+set.add(1);
+set.add(2);
+System.out.println(set.contains(1)); // return true
+System.out.println(set.contains(3)); // return false
+for (Integer element : set) {
+    System.out.println(element); // 1, 2
+}
+
+
+// TreeMap sorted key
+TreeMap<Integer, Integer> map2 = new TreeMap<>();
+map2.put(1, 1);
+map2.put(0, 0);
+map2.put(3, 3);
+map2.get(1);
+// lowerKey(), floorKey(), higherKey, ceilingKey()
+System.out.println(map2.firstKey());
+System.out.println(map2.lowerKey(1)); //return 0, < 1
+System.out.println(map2.higherKey(1)); //return 3, > 1
+System.out.println(map2.floorKey(1)); //return 1, <= 1
+System.out.println(map2.ceilingKey(1)); //return 1, >= 1
+
+
+// TreeSet
+TreeSet<Integer> treeSet = new TreeSet<>();
+treeSet.add(0);
+treeSet.add(1);
+System.out.println(treeSet.lower(1));
+System.out.println(treeSet.higher(1));
+System.out.println(treeSet.floor(1));
+
