be88e0413b90215c5b4922a304deb7e6aa6f4839
Zhengjian Kang <zk299@nyu.edu>
2023-03-09 16:04:43 -0500

    Update Reflection.java

--- javaSE/Reflection/Reflection.java
+++ javaSE/Reflection/Reflection.java
@@ -1,4 +1,4 @@
-// Class<?>: (wildcard ?) Java wildcard: ? for generic type
+// Class<?>: (wildcard ?) for generic type
 1. Class<Object> objectClass = object.getClass();
 2. Class<Car> carClass = Car.class;
   - ".class" suffix to a type name
@@ -6,19 +6,25 @@
 3. Class.forName("fullName")
 List<?>  <-- List<T> extends
 
-
+  
 // Constructor<?> class
 1. Class.getDeclaredConstructors()                       : include public or private
 2. Class.getConstructors()                               : include public only
 3. Class.getDeclaredConstructor(Class<?> ... paramType)  : include paramTypes
 
 // create an instance
-1. Class.getDeclaredConstructor(Class<?> ... paramType)
-2. Constructor.setAccessible(true);                      : private constructor
-3. Constructor.newInstance(Ojbect ... arguments)         : create a new instance
+1. constructor = class.getDeclaredConstructor(Class<?> ... paramType)
+   constructor = class.getDeclaredConstructor()          : default constructor
+2. constructor.setAccessible(true);                      : private constructor
+3. constructor.newInstance(Ojbect ... arguments)         : create a new instance
 
-// inspect fields
-1. Class.getDeclaredFields()                             : all fields but exclude inherited
+
+// inspect & modify fields
+1. class.getDeclaredFields();                             : all fields but exclude inherited
+2. class.getDeclaredField("fieldName");                   : a field with field name
+3. field.get(instance);                                   : get field value from an instance
+4. field.getType();                                       : get field type
+5. field.set(instance, value)
 
 
 
