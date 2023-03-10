ff214318022f134ed2de8f9a7396b223d3f429dd
Zhengjian Kang <zk299@nyu.edu>
2023-03-09 19:12:52 -0500

    Update Reflection.java

--- javaSE/Reflection/Reflection.java
+++ javaSE/Reflection/Reflection.java
@@ -3,26 +3,26 @@
 2. Class<Car> carClass = Car.class;
   - ".class" suffix to a type name
   - Class booleanType = boolean.class;
-3. Class.forName("fullName")
+3. Class.forName("package.full")
 List<?>  <-- List<T> extends
 
   
 // Constructor<?> class
-1. Class.getDeclaredConstructors()                       : include public or private
-2. Class.getConstructors()                               : include public only
-3. Class.getDeclaredConstructor(Class<?> ... paramType)  : include paramTypes
+1. clazz.getDeclaredConstructors()                       : include public or private
+2. clazz.getConstructors()                               : include public only
+3. clazz.getDeclaredConstructor(Class<?> ... paramType)  : include paramTypes
 
 // create an instance
-1. constructor = class.getDeclaredConstructor(Class<?> ... paramType)
-   constructor = class.getDeclaredConstructor()          : default constructor
+1. constructor = clazz.getDeclaredConstructor(Class<?> ... paramType)
+   constructor = clazz.getDeclaredConstructor()          : default constructor
 2. constructor.setAccessible(true);                      : private constructor
 3. constructor.newInstance(Ojbect ... arguments)         : create a new instance
 
 
 // inspect & modify fields
 // Refer to ConfigParser & JsonWritter
-1. class.getDeclaredFields();                             : all fields but exclude inherited
-2. class.getDeclaredField("fieldName");                   : a field with field name
+1. clazz.getDeclaredFields();                             : all fields but exclude inherited
+2. clazz.getDeclaredField("fieldName");                   : a field with field name
 3. field.get(instance);                                   : get field value from an instance
 4. field.getType();                                       : get field type
 5. field.set(instance, value)                             : set field value for instance
