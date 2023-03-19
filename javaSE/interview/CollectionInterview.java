4e8248e939f88f7a3bb863c48a6da4c0b2b2128c
Zhengjian Kang <zk299@nyu.edu>
2023-03-18 23:50:27 -0400

    Create CollectionInterview.java

---
+++ javaSE/interview/CollectionInterview.java
@@ -0,0 +1,24 @@
+// video https://www.youtube.com/watch?v=GO67C7V-IbQ&list=PLVz2XdJiJQxwS8FyWnWyKyfILxHPLsiro&index=8
+
+1. What all collection you used in your project?
+  List(I): ArrayList, LinkedList, CopyOnWrtieArrayList (concurrent)
+  Set(I): HashSet, LinkedHashSet, TreeSet, CopyOnWrtieArraySet (concurrent)
+  Map(I): HashMap, LinkedHashMap, TreeMap, ConcurrentHashMap (concurrent)
+
+2. What is the difference between list and set
+
+3. What is the difference betwwen ArrayList and LinkedList
+
+4. List object creation scenario
+
+5. Declaring a list field with the final keyword?
+  
+6. How can I write Custom ArrayList where I do not want to allow duplicate?
+
+7. Why Set does not allow duplicate element?
+  
+8. What is the difference between Comparable and Comparator?
+  Comparable: Collections.sort(List)
+  Comparator: Collections.sort(list, comparator)
+  
+9. 
