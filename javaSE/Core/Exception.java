e1623770c6614227842500225e7e3e9f17e705d6
Zhengjian Kang <zk299@nyu.edu>
2023-03-18 20:29:19 -0400

    Update Exception.java

--- javaSE/Core/Exception.java
+++ javaSE/Core/Exception.java
@@ -6,17 +6,19 @@
 
 
 // Checked exception
-// Checked exception must be rethrow (throws in method) or try catch
+// Checked exception must be re-throw (throws in method) or try catch
 public class MyException extends Exception {
   public MyException(String message) {
     super(message);
   }
 }
 
-
 // Unchecked exception
 // Unchecked can be throw but not need to be marked as throws in method
 public class MyException extends RuntimeException {
 }
+
+// 
+
 
 
