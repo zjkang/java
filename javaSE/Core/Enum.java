7523786455dc4e5158ffa1d4645f7c25b1035ce8
Zhengjian Kang <zk299@nyu.edu>
2023-03-28 20:46:52 -0400

    Update Enum.java

--- javaSE/Core/Enum.java
+++ javaSE/Core/Enum.java
@@ -1,3 +1,6 @@
+// https://www.baeldung.com/a-guide-to-java-enums
+// https://www.youtube.com/watch?v=eVv2CLoaRTw&list=PLmOn9nNkQxJG_AbAUeyAPH3fO0i_APAM9&index=117
+
 
 enum Season {
   //1. define enum object at the beginning
@@ -21,9 +24,74 @@ enum Season {
   String getSeasonDesp() {
     return searconDesp;
   }
+  
+  // other functions
+  void show() {
+    System.out.println("show");
+  }
 }
 
 // usage
 Season.SPRING.getClass();
 Season.SPRING.getClass().getSuperClass(); // Super class: Enum class, cannot extend other super class
+String toString(): override toString method, default returns constant name e.g., 'SPRING' in the example
+Static Season[] values(): Season.values() to iterate all enum type values
+Static Season valueOf(String name): convert name to enum type; the name must be enum type name 'SPRING', otherwise throw illegalArgumentException
+String name(): Season.SPRING.name() return 'SPRING'
+int ordinal(): Season.AUTUMN.ordinal(): return the order of enum AUTUMN;
+
+// enum
+1. enum type can implments an interface; override interface abstract method;
+2. enum type can implments an interface; override interface abstract method for each enum type object;
+
+interface Info {
+  void show();
+}
+
+enum Season {
+  //1. define enum object at the beginning
+  SPRING("Spring", "Spring 1") {
+    @Override
+    void show() {
+      System.out.println("show1");
+    }
+  },
+  SUMMER("Summer", "Summer 1") {
+    void show() {
+      System.out.println("show2");
+    }
+  },
+  AUTUMN("Autumn", "Autumn 1") {
+    void show() {
+      System.out.println("show3");
+    }
+  },
+  WINTER("Winter", "Winter 4") {
+    void show() {
+      System.out.println("show1");
+    }
+  };
+  
+  private final String seasonName;
+  private final String seasonDesp;
+  
+  private Season(String s, String d) {
+    this.seasonName = s;
+    this.seasonDesp = d;
+  }
+  
+  String getSeasonName() {
+    return seasonName;
+  }
+  
+  String getSeasonDesp() {
+    return searconDesp;
+  }
+  
+  // other functions
+}
+
+
+
+
 
