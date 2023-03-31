e61a43dd5919d8ffbaa6243238d90d9b7ee1c9a4
Zhengjian Kang <zk299@nyu.edu>
2023-03-31 19:24:50 -0400

    Update Enum.java

--- javaSE/Core/Enum.java
+++ javaSE/Core/Enum.java
@@ -1,5 +1,5 @@
 // https://www.baeldung.com/a-guide-to-java-enums
-// https://www.youtube.com/watch?v=eVv2CLoaRTw&list=PLmOn9nNkQxJG_AbAUeyAPH3fO0i_APAM9&index=117
+// 尚硅谷java: https://www.youtube.com/watch?v=eVv2CLoaRTw&list=PLmOn9nNkQxJG_AbAUeyAPH3fO0i_APAM9&index=117
 
 
 enum Season {
@@ -31,24 +31,27 @@ enum Season {
   }
 }
 
-// usage
-Season.SPRING.getClass();
-Season.SPRING.getClass().getSuperClass(); // Super class: Enum class, cannot extend other super class
-String toString(): override toString method, default returns constant name e.g., 'SPRING' in the example
-Static Season[] values(): Season.values() to iterate all enum type values
-Static Season valueOf(String name): convert name to enum type; the name must be enum type name 'SPRING', otherwise throw illegalArgumentException
-String name(): Season.SPRING.name() return 'SPRING'
-int ordinal(): Season.AUTUMN.ordinal(): return the order of enum AUTUMN;
+// usage & methods
+// Season.SPRING.getClass(); // return Season class
+// Season.SPRING.getClass().getSuperClass(); // Super class: Enum class, cannot extend other super class
+
+// Static Season[] values(): Season.values() to iterate all enum type values
+// Static Season valueOf(String name): Season.valueOf("SPRING") convert name to enum type; the name must be enum type name 'SPRING', otherwise throw illegalArgumentException
+
+// String toString(): override toString method, default returns constant name e.g., 'SPRING' in the example
+// String name(): Season.SPRING.name() return 'SPRING'
+// int ordinal(): Season.AUTUMN.ordinal(): return the order of enum AUTUMN
+
 
 // enum
-1. enum type can implments an interface; override interface abstract method;
-2. enum type can implments an interface; override interface abstract method for each enum type object;
+// 1. enum type can implments an interface; override interface abstract method in the "other functions"
+// 2. enum type can implments an interface; override interface abstract method for each enum type object; see the example below
 
 interface Info {
   void show();
 }
 
-enum Season {
+enum Season implements Info {
   //1. define enum object at the beginning
   SPRING("Spring", "Spring 1") { // call constructor
     @Override
@@ -92,11 +95,8 @@ enum Season {
 }
 
 
-
-// can use enum to create singleton class instance
+// Singleton use enum: can use enum to create singleton class instance
 enum COLOR {
   RED
 };
-
-
 
