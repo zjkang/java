e1945a038edd6cd444cc850d428420921f89b8f6
Zhengjian Kang <zk299@nyu.edu>
2023-04-14 22:04:38 -0400

    Create CompletableFuture.java

---
+++ javaSE/multiThread/CompletableFuture.java
@@ -0,0 +1,35 @@
+// Refer https://www.youtube.com/watch?v=GJ5Tx43q6KM
+
+// CompletableFuture tutorials
+// Compare with Futures, ExecutorService, Callback interface, Thread Pools
+
+ExecutorService service = Executors.newFixedThreadPool(10);
+Future<List<Integer>> feature = service.submit(() -> {
+  // doing api call, waiting...
+  Thread.sleep(10000);
+  return Arrays.asList(1,2,3,4);
+});
+
+// block some time
+List<Integer> list = feature.get();
+
+// 原有的async programming的问题:
+// 1. it cannot be manually completed
+// 2. multiple Futures cannot be chained together
+// 3. Cannot combine multiple Futures together
+// 4. No proper exception handling
+
+// 如果执行时间太长,可以通过complete manualy结束执行
+CompletableFuture<String> completableFuture = new CompletableFuture<>();
+completableFuture.get();
+completableFuture.complete("return some dummy data");
+
+
+runAsync() vs supplyAsync()
+  
+
+
+
+
+
+
