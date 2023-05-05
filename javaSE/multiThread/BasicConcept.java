b1f2f159a749a6b6f3ac85cabee45ed309c10a83
Zhengjian Kang <zk299@nyu.edu>
2023-05-04 22:45:49 -0400

    Update BasicConcept.java

--- javaSE/multiThread/BasicConcept.java
+++ javaSE/multiThread/BasicConcept.java
@@ -9,11 +9,15 @@
 
 
 /*
-Create Thread
+  创建新线程
+  
   Java用Thread对象表示一个线程，通过调用start()启动一个新线程；
   一个线程对象只能调用一次start()方法；
   线程的执行代码写在run()方法中；
   线程调度由操作系统决定，程序本身无法决定调度顺序；
+  
+  必须调用Thread实例的start()方法才能启动新线程，如果我们查看Thread类的源代码，会看到start()方法内部调用了一个private native void start0()方法，
+  native修饰符表示这个方法是由JVM虚拟机内部的C代码实现的，不是由Java代码实现的
 */
 // Method 1: inherit Thread
 public class Main {
