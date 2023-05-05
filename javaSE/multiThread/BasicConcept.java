e101733b23f4464e38a1ac668089d1f6c4e3f2a9
Zhengjian Kang <zk299@nyu.edu>
2023-05-04 23:29:09 -0400

    Update BasicConcept.java

--- javaSE/multiThread/BasicConcept.java
+++ javaSE/multiThread/BasicConcept.java
@@ -51,6 +51,35 @@ class MyRunnable implements Runnable {
 
 // ---------------------------------------
 
+/*
+  线程的状态
+
+  New：新创建的线程，尚未执行；
+  Runnable：运行中的线程，正在执行run()方法的Java代码；
+  Blocked：运行中的线程，因为某些操作被阻塞而挂起；
+  Waiting：运行中的线程，因为某些操作在等待中；
+  Timed Waiting：运行中的线程，因为执行sleep()方法正在计时等待；
+  Terminated：线程已终止，因为run()方法执行完毕。
+
+  Java线程对象Thread的状态包括：New、Runnable、Blocked、Waiting、Timed Waiting和Terminated；
+  通过对另一个线程对象调用join()方法可以等待其执行结束；
+  可以指定等待时间，超过等待时间线程仍然没有结束就不再等待；
+  对已经运行结束的线程调用join()方法会立刻返回
+*/
+
+
+/*
+中断线程
+
+对目标线程调用interrupt()方法可以请求中断一个线程，目标线程通过检测isInterrupted()标志获取自身是否已中断。
+如果目标线程处于等待状态，该线程会捕获到InterruptedException；
+
+目标线程检测到isInterrupted()为true或者捕获了InterruptedException都应该立刻结束自身线程；
+通过标志位判断需要正确使用volatile关键字；
+volatile关键字解决了共享变量在线程间的可见性问题。
+*/
+
+
 
 
 https://www.baeldung.com/java-wait-notify
